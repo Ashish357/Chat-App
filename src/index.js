ddb8b57a7168de841435414f47ccba9495fde30c
Ashish Saha <49359207+Ashish357@users.noreply.github.com>
2023-11-30 12:48:44 +0530

    Update index.js port

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
+const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
@@ -61,6 +62,6 @@ io.on('connection', (socket) => {
 
 })
 
-server.listen(3000,()=>{
+server.listen(port,()=>{
     console.log('server started');
 })
